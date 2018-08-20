ce9e48812c5208c4804fbf34504b26d5f8f35fa6
wangsb <2885129077@qq.com>
2018-08-20 16:30:37 +0800

    自定义注解

--- src/main/java/com/springboot/MongodbApplication.java
+++ src/main/java/com/springboot/MongodbApplication.java
@@ -8,7 +8,7 @@
 //import org.springframework.boot.autoconfigure.SpringBootApplication;
 //
 //@SpringBootApplication
-//public class SpringbootMongodbApplication implements CommandLineRunner {
+//public class MongodbApplication implements CommandLineRunner {
 //
 //
 //    @Autowired
